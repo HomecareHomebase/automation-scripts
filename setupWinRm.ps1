074fcfbdcfc7ab3b97e333f7f0b3eb986c4ee11b
Andy Stumph <andy.stumph@gmail.com>
2021-09-03 07:51:28 -0400

    Updated winrm script (#2)
    Co-authored-by: Andy Stumph <astumph@hchb.com>

--- setupWinRm.ps1
+++ setupWinRm.ps1
@@ -6,7 +6,9 @@ param (
 Add-LocalGroupMember -Group "Administrators" -Member "$svcAccount"
 
 $IP = (Get-NetIPAddress -InterfaceAlias "Ethernet*" -AddressFamily IPv4).IPAddress
-$Cert = Get-Certificate -Template WebServerExportPrivate -DnsName $IP,"$($Env:COMPUTERNAME).$(($Env:USERDNSDOMAIN).ToLower())" -SubjectName "CN=$($Env:COMPUTERNAME).$(($Env:USERDNSDOMAIN).ToLower())" -CertStoreLocation 'Cert:\LocalMachine\My\'
+$ShortName = "$($Env:COMPUTERNAME)"
+$FQDN = "$($Env:COMPUTERNAME).$(($Env:USERDNSDOMAIN).ToLower())"
+$Cert = Get-Certificate -Template WebServerExportPrivate -DnsName $IP,$ShortName -SubjectName "CN=$($FQDN)" -CertStoreLocation 'Cert:\LocalMachine\My\'
 $CertificateThumbprint = $Cert.Certificate.Thumbprint
 
 $listener = @{
