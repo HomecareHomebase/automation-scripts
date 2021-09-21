9bbeb7ccfcec85c276332f5d75976c9890971212
David Woods <drwoods21@gmail.com>
2021-09-21 12:31:41 -0500

    Add domain as a parameter to pass in

--- setupWinRm.ps1
+++ setupWinRm.ps1
@@ -1,13 +1,14 @@
 [cmdletbinding()]            
 param (
     [string]$svcAccount
+    [string]$domain
 )
 
 Add-LocalGroupMember -Group "Administrators" -Member "$svcAccount"
 
 $IP = (Get-NetIPAddress -InterfaceAlias "Ethernet*" -AddressFamily IPv4).IPAddress
 $ShortName = "$($Env:COMPUTERNAME)"
-$FQDN = "$($Env:COMPUTERNAME).$(($Env:USERDNSDOMAIN).ToLower())"
+$FQDN = "$($Env:COMPUTERNAME).$(($domain).ToLower())"
 $Cert = Get-Certificate -Template WebServerExportPrivate -DnsName $IP,$ShortName -SubjectName "CN=$($FQDN)" -CertStoreLocation 'Cert:\LocalMachine\My\'
 $CertificateThumbprint = $Cert.Certificate.Thumbprint
 
