eb4756ad97ecf256f516f31c7cf00a987b82e86a
Andy Stumph <astumph@hchb.com>
2021-10-01 12:13:16 -0400

    Added FQDN to SAN in the certificate.

--- setupWinRm.ps1
+++ setupWinRm.ps1
@@ -9,7 +9,7 @@ Add-LocalGroupMember -Group "Administrators" -Member "$svcAccount"
 $IP = (Get-NetIPAddress -InterfaceAlias "Ethernet*" -AddressFamily IPv4).IPAddress
 $ShortName = "$($Env:COMPUTERNAME)"
 $FQDN = "$($Env:COMPUTERNAME).$(($domain).ToLower())"
-$Cert = Get-Certificate -Template WebServerExportPrivate -DnsName $IP,$ShortName -SubjectName "CN=$($FQDN)" -CertStoreLocation 'Cert:\LocalMachine\My\'
+$Cert = Get-Certificate -Template WebServerExportPrivate -DnsName $IP,$ShortName,$FQDN -SubjectName "CN=$($FQDN)" -CertStoreLocation 'Cert:\LocalMachine\My\'
 $CertificateThumbprint = $Cert.Certificate.Thumbprint
 
 $listener = @{
