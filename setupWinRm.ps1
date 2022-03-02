6cc8bd3c02bc11d6a66e68f29beb28781fac3990
David Woods <drwoods21@gmail.com>
2022-03-02 15:12:18 -0600

    Add check for admin user. Add while loop for checking existing root cert

--- setupWinRm.ps1
+++ setupWinRm.ps1
@@ -1,12 +1,45 @@
-[cmdletbinding()]            
-param (
-    [string]$svcAccount,
-    [string]$domain
+#Requires -Version 5.0
+
+[CmdletBinding(SupportsShouldProcess = $True)]
+         
+param 
+(
+  [Parameter(Mandatory = $True, Position = 0)]
+  [ValidateNotNullOrEmpty()]
+  [String]
+  $svcAccount,
+
+  [Parameter(Mandatory = $True, Position = 1)]
+  [ValidateNotNullOrEmpty()]
+  [String]
+  $domain,
+
+  [Parameter(Mandatory = $True, Position = 2)]
+  [ValidateNotNullOrEmpty()]
+  [String]
+  $rootCertName
 )
 
 Start-Transcript -Path C:\Temp\setupWinRm.log
 
-Add-LocalGroupMember -Group "Administrators" -Member "$svcAccount"
+if (-not (Get-LocalGroupMember -Group "Administrators" -Member "*$($svcAccount)*" -ErrorAction SilentlyContinue)) {
+  Add-LocalGroupMember -Group "Administrators" -Member "$svcAccount"
+}
+
+while ($count -le 30) {
+  $existingCert = Get-ChildItem Cert:\LocalMachine\Root\ | Where-Object Subject -match $rootCertName
+
+  if (-not $existingCert) {
+    $count++
+    Start-Sleep -Seconds 1
+  } else {
+    break
+  }
+
+  if ($count -eq 30) {
+    throw "Failed to find root certificate $rootCertName"
+  }
+}
 
 $IP = (Get-NetIPAddress -InterfaceAlias "Ethernet0*" -AddressFamily IPv4).IPAddress
 $ShortName = "$($Env:COMPUTERNAME)"
