e4226aafc7f6c314cc72376f08b874d2dd98fad9
Tanner Watson <tannerwatson@gmail.com>
2022-06-08 13:45:33 -0400

    Added initial avd script

--- SetupAVD.ps1
+++ SetupAVD.ps1
@@ -1,27 +1,16 @@
-#{
-#    "commandToExecute":"powershell -ExecutionPolicy Unrestricted -Command { Expand-Archive -Path Configuration.zip -DestinationPath .\\Configuration\\ -Force; .\\Configuration\\Script-AddRdshServer.ps1 -HostPoolName \"${module.primary_avd_r2_host_pool.name}\" -RegistrationInfoToken \"${module.primary_avd_r2_host_pool.token}\" }",
-#    "fileUris":[
-#       "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration.zip"
-#    ]
-# }
+[CmdletBinding(SupportsShouldProcess = $True)]
+param 
+(
+  [Parameter(Mandatory = $True)]
+  [ValidateNotNullOrEmpty()]
+  [String]
+  $HostPoolName,
 
-# [CmdletBinding(SupportsShouldProcess = $True)]
-         
-# param 
-# (
-#   [Parameter(Mandatory = $True)]
-#   [ValidateNotNullOrEmpty()]
-#   [String]
-#   $HostPoolName,
-
-#   [Parameter(Mandatory = $True)]
-#   [ValidateNotNullOrEmpty()]
-#   [String]
-#   $RegistrationInfoToken
-# )
-
-$HostPoolName = ""
-$RegistrationInfoToken = ""
+  [Parameter(Mandatory = $True)]
+  [ValidateNotNullOrEmpty()]
+  [String]
+  $RegistrationInfoToken
+)
 
 $url = 'https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration.zip'
 $zipPath = ".\Configuration.zip"
@@ -32,11 +21,11 @@ $ScriptPath = [system.io.path]::GetDirectoryName($PSCommandPath)
 Write-Host "Script Path: $ScriptPath"
 
 Write-Host "Downloading $url to $zipPath"
-# Invoke-WebRequest $url -OutFile $zipPath
+Invoke-WebRequest $url -OutFile $zipPath
 
 Write-Host "Expanding $zipPath to $extractPath"
-# Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
+Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
 
 Write-Host "Executing $extractPath\Script-AddRdshServer.ps1"
 
-.\$($extractPath)\Script-AddRdshServer.ps1 -HostPoolName $HostPoolName -RegistrationInfoToken $RegistrationInfoToken
+& ".\$($extractPath)\Script-AddRdshServer.ps1" -HostPoolName $HostPoolName -RegistrationInfoToken $RegistrationInfoToken
